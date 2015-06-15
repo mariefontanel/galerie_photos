1e7d3823d2b9d2d61af79e1d58665ea7984dfba7
marieh <marieherelier@gmail.com>
2015-06-15 16:02:16 +0200

    ajout des routes de la galerie

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'galerie#index'
+  get 'upload' => 'galerie#new'
+  post 'upload' => 'galerie#create'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
