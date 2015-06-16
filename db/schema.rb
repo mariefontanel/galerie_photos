92f1fa3ac473d892e0672b3b639903b0a423fcfe
p-guerin <guerin.pierre26@gmail.com>
2015-06-16 11:18:31 +0200

    Modification du fichier config et de la bdd pour utiliser paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150615122935) do
+ActiveRecord::Schema.define(version: 20150616083939) do
 
   create_table "images", force: :cascade do |t|
     t.string   "titre"
     t.string   "lien"
     t.string   "tag"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
