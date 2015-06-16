16796107dcac93f2e0796215b433a8f061d37349
p-guerin <guerin.pierre26@gmail.com>
2015-06-16 11:15:30 +0200

    Controle de l'upload avec le controller galerie_controller et le model image

--- app/controllers/galerie_controller.rb
+++ app/controllers/galerie_controller.rb
@@ -1,12 +1,27 @@
 class GalerieController < ApplicationController
 	def index
+    @image = Image.all
 	end
 
 	def new
+    @image = Image.new
 	end
 
 	def create
-		
+		@image = Image.create(params_image)
+    if @image.save
+      flash[:success] = "Votre image a bien été ajoutée dans la galerie."
+      redirect_to root_path
+    else
+      flash[:error] = "Une erreur est survenue."
+      render 'new'
+    end
 	end
+
+  private
+
+  def params_image
+    params.require(:upload).permit(:titre, :tag, :avatar)
+  end
 
 end
