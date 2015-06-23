63751a03d85ae117d32837184fb95bf1f53ad736
p-guerin <guerin.pierre26@gmail.com>
2015-06-23 13:08:03 +0200

    Force le resize sur les images upload

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ActiveRecord::Base
   validates :titre, :tag, presence: true
   
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x200!", :thumb => "100x66!" }, :default_url => "/images/:style/missing.png"
   validates_attachment :avatar, :presence => true, :content_type => { :content_type => ["image/jpeg", "image/png", "image/gif", "image/svg"] }, :size => { :in => 0..5.megabytes }
 end
